a779f232d6b9d4053a81a97066508394c043d209
Ayushi Pandey <40225444+ayushipandey01@users.noreply.github.com>
2023-02-02 22:14:07 +0530

    created albums using html in javascript

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const imageURL = document.getElementById("img");
-// console.log(imageURL);
-
-const imageDescription = document.getElementById("description");
-// console.log(imageDescription);
+const albums = document.getElementsByClassName('albums')[0];
 
 const addAlbum = document.getElementById("addAlbum");
-const container = document.getElementsByClassName("container")[0];
 
 addAlbum.addEventListener("click" ,(e) => {
     e.preventDefault();
-    var image = imageURL.value;
-    var imgDescription = imageDescription.value;
-    const createAlbum = createAlbum(image , imgDescription);
-    albums.appendChild(createAlbum);
+    const image = document.getElementById("img").value;
+    const imgDescription = document.getElementById("description").value;
+    const Album = createAlbum(image , imgDescription);
+    albums.appendChild(Album);
 })
 
 
 function createAlbum (imageURL,imageDescription){
-    const albumdiv = document.createElement("div");
-    albumdiv.className = "album" ;
+    const albumdiv = document.createElement('div');
+    albumdiv.className = 'album' ;
 
-    const image = document.createElement("img");
-    image.setAttribute("src" , imageURL);
+    const image = document.createElement('img');
+    image.setAttribute('src' , imageURL);
 
-    const para = document.createElement("p");
+    const para = document.createElement('p');
+    para.className = 'para';
     para.textContent = imageDescription;
 
-    const albumFooter = createElement("div");
-    albumFooter.className = "albumFooter";
+    const albumFooter = document.createElement('div');
+    albumFooter.className = 'albumFooter';
     
-    const buttonDiv = createElement("div");
-    const button1 = createElement("button");
-    const button2 = createElement("button");
+    const buttonDiv = document.createElement('div');
+    const button1 = document.createElement('button');
+    const button2 = document.createElement('button');
 
     button1.textContent = "View";
     button2.textContent = "Edit";
@@ -39,14 +35,15 @@ function createAlbum (imageURL,imageDescription){
     buttonDiv.appendChild(button1);
     buttonDiv.appendChild(button2);
 
-    const timer = document.createElement("span");
+    const timer = document.createElement('span');
     timer.textContent = "9 Mins";
 
     albumFooter.appendChild(buttonDiv);
     albumFooter.appendChild(timer);
 
-    albumdiv.appendChild(para);
+    
     albumdiv.appendChild(image);
+    albumdiv.appendChild(para);
     albumdiv.appendChild(albumFooter);
 
     return albumdiv;
